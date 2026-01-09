a9c763692ef8242a147009f5c19fb3814eede019
Brayan <bittolo.8bit@gmail.com>
2026-01-09 18:26:16 +0100

    fourth exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function vowelsCount(word) {
+  let counter = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if ("aeiou".includes(word[i].toLowerCase())) {
+      counter++;
+    }
+  }
+  return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(vowelsCount(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
